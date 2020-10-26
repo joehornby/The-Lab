df6506e3a62b8bb8d11ea004fccabc52b4ad21ec
Joseph Hornby <joehornby@gmail.com>
2020-10-26 15:13:04 +0000

    add images

---
+++ scrolling/src/images/images.js
@@ -0,0 +1,11 @@
+const images = [
+  "/images/process_1.jpg",
+  "/images/process_2.jpg",
+  "/images/process_3.jpg",
+  "/images/process_4.jpg",
+  "/images/process_5.jpg",
+  "/images/process_6.jpg",
+  "/images/process_7.jpg"
+]
+
+export default images;
