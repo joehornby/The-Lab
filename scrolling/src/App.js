8dee5d6747b76f0b1bd2739ff49f934762ba052f
Joseph Hornby <joehornby@gmail.com>
2020-10-27 10:07:04 +0000

    add window resize handler

--- scrolling/src/App.js
+++ scrolling/src/App.js
@@ -1,11 +1,29 @@
+import React, { useRef, useEffect } from "react"
 import "./App.scss"
+
+// import window size hook
+import useWindowSize from './hooks/useWindowSize'
 
 import images from "./images/images"
 
 function App() {
+  // Hook
+  const size = useWindowSize()
+
+  // Ref
+  const app = useRef()
+  const scrollContainer = useRef()
+
+  // Using empty dependency [], useEffect will only run once component has mounted. 
+  // With a dependency, it will also run when the dependency changes - in this case, when the the window size changes.
+  // TODO: add debounce to avoid excessive rendering on window size
+  useEffect(() => {
+    document.body.style.height = `${scrollContainer.current.getBoundingClientRect().height}px`
+  }, [size.height]) // [] = empty dependency, [size.height] = window size hook imported above
+
   return (
-    <div className="App">
-      <div className="scroll">
+    <div ref={app} className="App">
+      <div ref={scrollContainer} className="scroll">
         {images.map((image, index) => (
           <>
             <div key={index} className="img-container">
